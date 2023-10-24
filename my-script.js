16ebf67cbd53b573fd1e9da6e81615102676d350
Dok6857 <141351808+Dok6857@users.noreply.github.com>
2023-10-24 19:04:40 +0300

    added button interaction

--- my-script.js
+++ my-script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function sum(first, second) {
+  return first + second;
+}
+
+function getNumber(x, y) {
+  //   const x = 5;
+  //   const y = 10;
+
+  const result = x * y;
+
+  return result;
+}
+
+const openButton = document.getElementById('openButton');
+const serviceElement = document.getElementById('service');
+
+openButton.addEventListener('click', function () {
+  serviceElement.classList.add('is-open');
+});
